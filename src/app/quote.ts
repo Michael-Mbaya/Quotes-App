b92c1c94793d35f0560dff29e938c993f68d566d
Michael-Mbaya <gikundamike@gmail.com>
2020-08-22 02:12:33 +0300

    quotes class and componet

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,10 @@
 export class Quote {
+    constructor(
+        public TextQuote: string,
+        public AuthorOfQuote: string,
+        public PostedBy: string,
+        public PostDate: Date,
+        public upvotes: number,
+        public downvotes: number
+      ) {  }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
-  constructor() { }
+  quotes = [
+    new Quote("If you cannot laugh at yourself then qwertyuioasdfghjkldfcvghb can't share Humor", 'Jim Carrey', 'Admin', new Date(2018, 4, 27, 23, 1), 0, 0),
+    new Quote("From you believe, so you shall achieve", 'Me', 'Admin', new Date(2018, 4, 27, 23, 1), 0, 0),
+    new Quote('Life is what You make it', 'Vybz kartel', 'Me', new Date(2018, 4, 27, 23, 5), 0, 0),
+    new Quote('Its always the nizzle my Shizzle', 'Snoop Dogg', 'Main Moi', new Date(2018, 4, 27, 23, 8), 0, 0),
+    new Quote('Its always the nizzle my Shizzle', 'Snoop Dogg', 'Main Moi', new Date(2018, 4, 27, 23, 8), 0, 0),
+    new Quote('Its always the nizzle my Shizzle', 'Snoop Dogg', 'Main Moi', new Date(2018, 4, 27, 23, 8), 0, 0),
+    new Quote('Siwezi Wacha Vela', 'Zzero Sufuri', 'Ombitho', new Date(2018, 4, 28, 7, 42), 0, 0)
+  ];
+
+  // constructor() { }
 
   ngOnInit(): void {
   }
