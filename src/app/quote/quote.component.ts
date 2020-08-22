6624b8bbb239e03afcf849759c12e5ab8d2095cc
Michael-Mbaya <gikundamike@gmail.com>
2020-08-23 01:59:28 +0300

    favicon.ico notes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,17 +13,10 @@ export class QuoteComponent implements OnInit {
     new Quote("From you believe, so you shall achieve", 'Me', 'Admin', new Date(2020, 6, 21), 0, 0),
     new Quote('Life is what You make it', 'Vybz kartel', 'Me', new Date(2020, 7, 12), 0, 0),
     new Quote('Its always the nizzle my Shizzle', 'Snoop Dogg', 'Main Moi', new Date(2019, 11, 31), 0, 0),
-    new Quote('Its always the nizzle my Shizzle', 'Snoop Dogg', 'Main Moi', new Date(2020, 11, 14), 0, 0),
     new Quote('Its always the nizzle my Shizzle', 'Snoop Dogg', 'Main Moi', new Date(2015, 10, 10), 0, 0),
     new Quote('Siwezi Wacha Vela', 'Zzero Sufuri', 'Ombitho', new Date(2010, 11, 12), 0, 0),
     new Quote('Siwezi Wacha Vela', 'Zzero Sufuri', 'Ombitho', new Date(2020, 7, 22), 0, 0)
   ];    //for dates, months counted from 0 i.e 0 is Jan to 11 is Dec
-
-  // deleteQuote(isComplete, index){
-  //   if (isComplete) {
-  //     this.quotes.splice(index,1);
-  //   }
-  // }
 
   deleteQuote(isComplete, index){
     if (isComplete) {
