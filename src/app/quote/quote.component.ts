7508880b669ed0bc51880c6064656aacd9fd7de5
Michael-Mbaya <gikundamike@gmail.com>
2020-08-25 10:29:35 +0300

    upvotes and downvotes intial zeroes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,13 +10,13 @@ import {  Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote("If you cannot laugh at yourself then you really can't share Humor with others.", 'Jim Carrey', 'Admin', new Date(2018, 4, 27, 23, 1), 27, 1),
-    new Quote("From you believe, so you shall achieve", 'Etana', 'Admin', new Date(2020, 6, 21), 35, 3),
-    new Quote("Life is what You make it", 'Vybz kartel', 'Thomas', new Date(2020, 7, 12), 16, 6),
-    new Quote("Its always the Shizzle to my Nizzle", 'Snoop Dogg', 'Main Moi', new Date(2019, 11, 31), 4, 20),
-    new Quote("Hustle hard till you don't have to Introduce yourself", 'Snoop Dogg', 'Main Moi', new Date(2015, 10, 10), 22, 3),
-    new Quote('Siwezi Wacha Rhumba', 'Sauti Sol', 'Sol Generation', new Date(2010, 11, 12), 13, 1),
-    new Quote('Ata kama ni Mita me sita-Bargain!', 'Rekless', 'Gengetoner', new Date(2020, 7, 2), 2, 0)
+    new Quote("If you cannot laugh at yourself then you really can't share Humor with others.", 'Jim Carrey', 'Admin', new Date(2018, 4, 27, 23, 1), 0, 0),
+    new Quote("From you believe, so you shall achieve", 'Etana', 'Admin', new Date(2020, 6, 21), 0, 0),
+    new Quote("Life is what You make it", 'Vybz kartel', 'Thomas', new Date(2020, 7, 12), 0, 0),
+    // new Quote("Its always the Shizzle to my Nizzle", 'Snoop Dogg', 'Main Moi', new Date(2019, 11, 31), 0, 0),
+    new Quote("Hustle hard till you don't have to Introduce yourself", 'Snoop Dogg', 'Main Moi', new Date(2015, 10, 10), 0, 0),
+    new Quote('Siwezi Wacha Rhumba', 'Sauti Sol', 'Sol Generation', new Date(2010, 11, 12), 0, 0),
+    new Quote('Ata kama ni Mita me sita-Bargain!', 'Rekless', 'Gengetoner', new Date(2020, 7, 2), 0, 0)
   ];    //for dates, months counted from 0 i.e 0 is Jan to 11 is Dec
 
   addNewQuote(quote){
